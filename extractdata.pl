1ccf7b9423bb855b83debb1379f134e37081b92b
Pato Sáinz <superpatosainz@gmail.com>
2015-05-25 20:26:12 -0400

    this is jsond enough

--- extractdata.pl
+++ extractdata.pl
@@ -27,9 +27,9 @@ $TableExtract->parse($raw);
 foreach my $TableSearch ( $TableExtract->tables ) {
 
 
-#	my $test = $TableSearch->rows->[1]->[2];
+	# my $test = $TableSearch->rows->[1]->[2];
 
-# Por cada fila
+	# Por cada fila
 	foreach my $rows ( $TableSearch->rows ) {
 
 		our $currentasig;
@@ -39,23 +39,15 @@ foreach my $TableSearch ( $TableExtract->tables ) {
 
 		foreach (@$rows) { $_ = trim($_); } # Normalizar los campos de texto
 
-# Sacamos la asignatura y las notas del primer semestre
+		# Sacamos la asignatura y las notas del primer semestre
 		if ($rows->[0] eq '1') {
 
 			# r1: asignatura
 			# r2-r16: notas
+			# r19: síntesis
 
 			$currentasig = $rows->[1];
 
-
-=comment
-			print "$rows->[1]:\n\n".
-			"1er Semestre: $rows->[2] $rows->[3] $rows->[4] $rows->[5] $rows->[6] ".
-			"$rows->[7] $rows->[8] $rows->[9] $rows->[10] $rows->[11] $rows->[12] ".
-			"$rows->[13] $rows->[14] $rows->[15] $rows->[16]\n".
-			"1er Semestre Síntesis: $rows->[19]\n";
-
-=cut
 			my @notasprimer;
 			if ($rows->[2] ne '-') { push(@notasprimer, $rows->[2]); }
 			if ($rows->[3] ne '-') { push(@notasprimer, $rows->[3]); }
@@ -75,17 +67,38 @@ foreach my $TableSearch ( $TableExtract->tables ) {
 
 
 			$notas{$currentasig}{'primersemestre'}{'notas'} = [@notasprimer];
+			$notas{$currentasig}{'primersemestre'}{'sintesis'} = undef;
+
+			if ($rows->[19] ne '-') { $notas{$currentasig}{'primersemestre'}{'sintesis'} = $rows->[19]; }
 
 		}
 
-# Luego las del segundo semestre
+		# Luego las del segundo semestre
 		if ($rows->[0] eq '2') {
 
+			my @notassegundo;
+			if ($rows->[2] ne '-') { push(@notassegundo, $rows->[2]); }
+			if ($rows->[3] ne '-') { push(@notassegundo, $rows->[3]); }
+			if ($rows->[4] ne '-') { push(@notassegundo, $rows->[4]); }
+			if ($rows->[5] ne '-') { push(@notassegundo, $rows->[5]); }
+			if ($rows->[6] ne '-') { push(@notassegundo, $rows->[6]); }
+			if ($rows->[7] ne '-') { push(@notassegundo, $rows->[7]); }
+			if ($rows->[8] ne '-') { push(@notassegundo, $rows->[8]); }
+			if ($rows->[9] ne '-') { push(@notassegundo, $rows->[9]); }
+			if ($rows->[10] ne '-') { push(@notassegundo, $rows->[10]); }
+			if ($rows->[11] ne '-') { push(@notassegundo, $rows->[11]); }
+			if ($rows->[12] ne '-') { push(@notassegundo, $rows->[12]); }
+			if ($rows->[13] ne '-') { push(@notassegundo, $rows->[13]); }
+			if ($rows->[14] ne '-') { push(@notassegundo, $rows->[14]); }
+			if ($rows->[15] ne '-') { push(@notassegundo, $rows->[15]); }
+			if ($rows->[16] ne '-') { push(@notassegundo, $rows->[16]); }
 
-			print "2do Semestre: $rows->[2] $rows->[3] $rows->[4] $rows->[5] $rows->[6] ".
-			"$rows->[7] $rows->[8] $rows->[9] $rows->[10] $rows->[11] $rows->[12] ".
-			"$rows->[13] $rows->[14] $rows->[15] $rows->[16]\n".
-			"2do Semestre Síntesis: $rows->[19]"."\n\n";
+
+			$notas{$currentasig}{'segundosemestre'}{'notas'} = [@notassegundo];
+			$notas{$currentasig}{'segundosemestre'}{'sintesis'} = undef;
+
+			if ($rows->[19] ne '-') { $notas{$currentasig}{'segundosemestre'}{'sintesis'} = $rows->[19]; }
+
 		}
 		
 
